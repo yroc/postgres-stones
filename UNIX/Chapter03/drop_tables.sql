7e0843957a42b6d0b0bab6bfd998b86944bf8d90
Cory Sand <yrocsand@gmail.com>
2017-01-15 23:47:29 -0500

    Remove drop sequence commands (legacy code related to resetting auto incrementing serial columns)

--- UNIX/Chapter03/drop_tables.sql
+++ UNIX/Chapter03/drop_tables.sql
@@ -9,9 +9,3 @@ drop table orderinfo;
 drop table item;
 
 drop table customer;
-
-drop sequence customer_customer_id_seq;
-
-drop sequence item_item_id_seq;
-
-drop sequence orderinfo_orderinfo_id_seq;
