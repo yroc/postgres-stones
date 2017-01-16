c5e7a0cbe6e33fa25db7e12833f2c269afdba839
Cory Sand <yrocsand@gmail.com>
2017-01-15 23:41:01 -0500

    Modify Date format of date_placed and date_shipped columns of orderinfo table

--- UNIX/Chapter03/pop-all-tables.sql
+++ UNIX/Chapter03/pop-all-tables.sql
@@ -61,11 +61,11 @@ insert into stock(item_id, quantity) values(10,1);
 
 -- Order info
 
-insert into orderinfo(customer_id, date_placed, date_shipped, shipping) values(3,'03-13-2004','03-17-2004', 2.99);
-insert into orderinfo(customer_id, date_placed, date_shipped, shipping) values(8,'06-23-2004','06-24-2004', 0.00);
-insert into orderinfo(customer_id, date_placed, date_shipped, shipping) values(15,'09-02-2004','09-12-2004', 3.99);
-insert into orderinfo(customer_id, date_placed, date_shipped, shipping) values(13,'09-03-2004','09-10-2004', 2.99);
-insert into orderinfo(customer_id, date_placed, date_shipped, shipping) values(8,'07-21-2004','07-24-2004', 0.00);
+insert into orderinfo(customer_id, date_placed, date_shipped, shipping) values(3,'2004-03-13','2004-03-17', 2.99);
+insert into orderinfo(customer_id, date_placed, date_shipped, shipping) values(8,'2004-06-23','2004-06-24', 0.00);
+insert into orderinfo(customer_id, date_placed, date_shipped, shipping) values(15,'2004-09-02','2004-09-12', 3.99);
+insert into orderinfo(customer_id, date_placed, date_shipped, shipping) values(13,'2004-09-03','2004-09-10', 2.99);
+insert into orderinfo(customer_id, date_placed, date_shipped, shipping) values(8,'2004-07-21','2004-07-24', 0.00);
 
 -- Order line
 
